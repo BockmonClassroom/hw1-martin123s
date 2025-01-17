64f740768c5d0cf6184c1f1996e8e89b79a9a548
martin123s <martin.xzf@gmail.com>
2025-01-16 20:32:31 -0500

    use pandas to get  data

--- printCSV.py
+++ printCSV.py
@@ -1,7 +1,18 @@
 # 01/14/2025
 # Shuiming Chen
 
+# import pandas to print data
+import pandas as pd
 
+# get the data path
+file_path = "./data/Iris.csv"
+
+# get the data and print out
+data = pd.read_csv(file_path)
+print(data)
+
+
+'''
 import csv
 
 # get the data path
@@ -14,3 +25,4 @@ with open(file_path, mode='r') as f:
     # print out the data line by line
     for row in csv_lines:
         print(row)
+'''
